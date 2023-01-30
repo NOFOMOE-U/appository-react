6dcb0d591bf5abee11b635e27daeb85bc872536a
Marcus Jones <indelibleink84@gmail.com>
2023-01-30 01:08:21 -0600

    finished setting up core graphql/apollo

--- libs/core/src/lib/core.module.ts
+++ libs/core/src/lib/core.module.ts
@@ -1,17 +1,25 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { GraphQLModule } from "@nestjs/graphql";
 import { configuration } from './config/configuration';
 import { validationSchema } from './config/validation';
+import { CoreResolver } from "./core.resolver";
+
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
       load: [configuration],
-      validationSchema
+      validationSchema,
+    }),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: 'schema.gql'
     })
   ],
   controllers: [],
-  providers: [],
+  providers: [CoreResolver],
   exports: [],
 })
 export class CoreModule {}
