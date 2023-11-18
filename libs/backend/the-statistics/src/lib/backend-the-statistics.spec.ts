ee45804271acaf8be1dcc46ae427f3fe6f4521f5
Marcus Jones <indelibleink84@gmail.com>
2023-11-18 11:53:43 -0600

    still reconfiguring user layer to separate secure info; accelevel; added: libraries: the-statistics, used:python; created: more doc tools, viewports,video

---
+++ libs/backend/the-statistics/src/lib/backend-the-statistics.spec.ts
@@ -0,0 +1,7 @@
+import { backendTheStatistics } from './backend-the-statistics'
+
+describe('backendTheStatistics', () => {
+  it('should work', () => {
+    expect(backendTheStatistics()).toEqual('backend-the-statistics')
+  })
+})
---
+++ libs/backend/the-statistics/src/lib/backend-the-statistics.ts
@@ -0,0 +1,3 @@
+export function backendTheStatistics(): string {
+  return 'backend-the-statistics'
+}
